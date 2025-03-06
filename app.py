67fec860bf932f9b9da3a99db39ac079ee16182a
mtheocharous <theocharous.m95@gmail.com>
2025-03-06 21:48:58 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello! Your Flask app is live on Render."
+
+@app.route('/run-script')
+def run_script():
+    result = sum(range(1, 101))  # Example script: Sum of 1 to 100
+    return f"Script executed! The result is: {result}"
+
+if __name__ == '__main__':
+    app.run()
