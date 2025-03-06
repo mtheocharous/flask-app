be32c3d98b3c22b647b0c85334bc34f71d037fd4
mtheocharous <theocharous.m95@gmail.com>
2025-03-06 22:08:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello! Your Flask app is live on Render."
+    return "Hello Irene <3! Your Flask app is live on Render."
 
 @app.route('/run-script')
 def run_script():
